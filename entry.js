e55f00d95fbff5986f41b5d547f70a61831e73b0
flyweights <flyweights@163.com>
2016-10-09 21:27:30 +0800

    webpack简单实践
    希望之后，可以有所补充

---
+++ entry.js
@@ -0,0 +1,1 @@
+require('./src/helloworld.js');
---
+++ src/helloworld.js
@@ -0,0 +1,17 @@
+'use strict';
+//引入相关的包
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+//es6语法
+class HelloWorld extends Component{
+    render(){
+       return(
+            <h1>hello,world</h1>
+        )
+   }
+}
+//渲染虚拟dom
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('app')
+);
